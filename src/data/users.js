f857c8a2848d47061902517962ac8c92fdbed7fa
HAKI <96266332+harleiaki@users.noreply.github.com>
2023-04-28 14:12:14 -0300

    Update users.js

--- src/data/users.js
+++ src/data/users.js
@@ -3,21 +3,21 @@ export default [
         id: 1,
         name: 'J',
         email: 'j@email.com',
-        avatar: 'https://pixabay.com/images/id-1388937.jpg'
+        avatar: 'https://cdn.pixabay.com/photo/2016/03/31/20/31/amazed-1295833_960_720.png'
     }, {
         id: 2,
         name: 'M',
         email: 'm@email.com',
-        avatar: 'https://cdn.pixabay.com/photo/2014/08/21/14/51/dog-423398_1280.jpg'
+        avatar: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_640.png'
     }, {
         id: 3,
         name: 'J',
         email: 'j@email.com',
-        avatar: 'https://cdn.pixabay.com/photo/2018/01/09/11/04/dog-3071334_1280.jpg'
+        avatar: 'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_640.png'
     }, {
         id: 4,
         name: 'R',
         email: 'r@email.com',
-        avatar: 'https://cdn.pixabay.com/photo/2019/09/10/09/23/dog-4465690_1280.jpg'
+        avatar: 'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_640.png'
     }
 ];
